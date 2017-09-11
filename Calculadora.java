37c2a2dbac4a4f2f37cf2b712bc9a47f4d593d0e
Ibathex <31348541+Ibathex@users.noreply.github.com>
2017-09-10 18:31:54 -0600

    Ejemplo clase Calculadora

---
+++ Calculadora.java
@@ -0,0 +1,12 @@
+package org.eclipse.che.examples;
+
+public class Calculadora {
+     public static void main(String... argvs){
+         int x = 10;
+         int y = 20;
+         int suma = x*y;
+         System.out.println("La suma de " + x + " y " + y + " = " + suma);
+ }
+
+
+}
